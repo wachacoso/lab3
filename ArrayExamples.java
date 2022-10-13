452dcfa5d0063b26e95c4dcffc7fd0015939c9dd
wachacoso <wachacoso@ucsd.edu>
2022-10-13 16:59:57 -0700

    ArrayExamples Lab 3

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -4,8 +4,9 @@ public class ArrayExamples {
 
   // Changes the input array to be in reversed order
   static void reverseInPlace(int[] arr) {
+    int[] array = reversed(arr);
     for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = arr[arr.length - i - 1];
+      arr[i] = array[i]; 
     }
   }
 
@@ -14,9 +15,9 @@ public class ArrayExamples {
   static int[] reversed(int[] arr) {
     int[] newArray = new int[arr.length];
     for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = newArray[arr.length - i - 1];
+      newArray[i] = arr[arr.length - i - 1]; 
     }
-    return arr;
+    return newArray; 
   }
 
   // Averages the numbers in the array (takes the mean), but leaves out the
--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,19 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReversedMany() {
+    int[] input1 = {1, 2, 5, 6, 3, 9};
+    int[] expected = {9, 3, 6, 5, 2, 1};
+    assertArrayEquals(expected, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversedPlaceMany() {
+    int[] input1 = {1, 2, 5, 6, 3, 9};
+    int[] expected = {9, 3, 6, 5, 2, 1};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(expected, input1);
+  }
 }
